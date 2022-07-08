f3ac6b13364d93fe78d659b211abf2dc878a9603
SuhasKajawe <108925489+SuhasKajawe@users.noreply.github.com>
2022-07-08 15:37:20 +0530

    First Commit
    my 1st Repository .

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApp.Models
+{
+    [Serializable]
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string location { get; set; }
+        public string department { get; set; }
+        public double salery { get; set; }
+
+    }
+}
